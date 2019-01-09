66820940a60140214eec0705a88268d26bc7b5ac
Robyn C. Allen <robyninboston@gmail.com>
2019-01-09 14:10:07 -0800

    fake improvements to move_left method

--- fish_swimming.py
+++ fish_swimming.py
@@ -30,29 +30,29 @@ food.showturtle()
 # a function which moves a piece of food
 # randomly around the screen
 def move_food():
-	# get the current coordinates
-	x_now = food.xcor()
-	y_now = food.ycor()
-	# make random motion
-	x_step = random.randint(-1,1)
-	y_step = random.randint(-1,1)
-	# set new coordinates
-	x_next = x_now + x_step
-	y_next = y_now + y_step
+    # get the current coordinates
+    x_now = food.xcor()
+    y_now = food.ycor()
+    # make random motion
+    x_step = random.randint(-1,1)
+    y_step = random.randint(-1,1)
+    # set new coordinates
+    x_next = x_now + x_step
+    y_next = y_now + y_step
 
-	# check to make sure the new x and y values are on the screen
-	if x_next > MIN_X and x_next < MAX_X:
-		x = x_next
-	else:
-		x = x_now
+    # check to make sure the new x and y values are on the screen
+    if x_next > MIN_X and x_next < MAX_X:
+        x = x_next
+    else:
+        x = x_now
 
-	if y_next > MIN_Y and y_next < MAX_Y:
-		y = y_next
-	else:
-		y = y_now
-	
-	# move the food to the new position	
-	food.goto(x,y)
+    if y_next > MIN_Y and y_next < MAX_Y:
+        y = y_next
+    else:
+        y = y_now
+    
+    # move the food to the new position 
+    food.goto(x,y)
 
 # define the player
 player = turtle.clone()
@@ -63,30 +63,31 @@ player.showturtle()
 # define the key bindings
 
 def move_right():
-	if player.xcor() < MAX_X:
-		x,y = player.pos()
-		player.goto(x+5,y)
+    if player.xcor() < MAX_X:
+        x,y = player.pos()
+        player.goto(x+5,y)
 
 turtle.onkeypress(move_right,"Right")
 
 def move_left():
-	if player.xcor() > MIN_X:
-		x,y = player.pos()
-		player.goto(x-5,y)
+    if player.xcor() > MIN_X:
+        a = 10
+        x,y = player.pos()
+        player.goto(x-20,y)
 
 turtle.onkeypress(move_left,"Left")
 
 def move_down():
-	if player.xcor() > MIN_Y:
-		x,y = player.pos()
-		player.goto(x,y-5)
+    if player.xcor() > MIN_Y:
+        x,y = player.pos()
+        player.goto(x,y-5)
 
 turtle.onkeypress(move_down,"Down")
 
 def move_up():
-	if player.xcor() < MAX_Y:
-		x,y = player.pos()
-		player.goto(x,y+5)
+    if player.xcor() < MAX_Y:
+        x,y = player.pos()
+        player.goto(x,y+5)
 
 turtle.onkeypress(move_up,"Up")
 
@@ -97,17 +98,17 @@ text1.goto(-200,100)
 text1.write("Press Space To Start The Game ", font=('Arial', 23, 'normal'))
 
 def check_collision():
-	# write your own code here!!!
-	# can you figure out how to check whether the player
-	# has collided with the food or not?
-	# BONUS: how would you keep score?
-	pass
+    # write your own code here!!!
+    # can you figure out how to check whether the player
+    # has collided with the food or not?
+    # BONUS: how would you keep score?
+    pass
 
 # new sprites can be animated by adding function calls inside play_game()
 def play_game():
-	move_food()
-	check_collision()
-	turtle.ontimer(play_game,0)
+    move_food()
+    check_collision()
+    turtle.ontimer(play_game,0)
 
 play_game()
 turtle.listen()
